e1f6a7b1ffe8af090e3a983ca33146e4da3719aa
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-09-12 09:53:28 -0300

    chore: setup config module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
 import { AppModule } from './app.module'
+import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3333)
+
+  // true -> diz ao Nest que a validação das envs foi feita e não podem ser undefined
+  const configService: ConfigService<Env, true> = app.get(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
 
 bootstrap()
