bb47e3c119c13d2d0566f97efccdfcb04e966940
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-11-24 08:46:11 -0300

    refactor: usage of usecase on authenticate controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -6,15 +6,23 @@ import { ListRecentQuestionsController } from './controllers/list-recent-questio
 import { DatabaseModule } from '../database/database.module'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { ListRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/list-recent-questions'
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
+import { CryptographyModule } from '../cryptography/cryptography.module'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     ListRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase, ListRecentQuestionsUseCase],
+  providers: [
+    CreateQuestionUseCase,
+    ListRecentQuestionsUseCase,
+    AuthenticateStudentUseCase,
+    RegisterStudentUseCase,
+  ],
 })
 export class HttpModule {}
