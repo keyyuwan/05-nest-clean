e4dd757888029aefb7493a7c3b18b2589fbaa887
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-12-07 10:07:25 -0300

    refactor: persist attachments on answers in the infra layer

--- src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
+++ src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
@@ -8,6 +8,8 @@ import { StudentFactory } from 'test/factories/make-student'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { QuestionFactory } from 'test/factories/make-question'
 import { AnswerFactory } from 'test/factories/make-answer'
+import { AttachmentFactory } from 'test/factories/make-attachment'
+import { AnswerAttachmentFactory } from 'test/factories/make-answer-attachment'
 
 describe('Edit answer (E2E)', () => {
   let app: INestApplication
@@ -15,12 +17,20 @@ describe('Edit answer (E2E)', () => {
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
   let answerFactory: AnswerFactory
+  let attachmentFactory: AttachmentFactory
+  let answerAttachmentFactory: AnswerAttachmentFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AnswerFactory,
+        AttachmentFactory,
+        AnswerAttachmentFactory,
+      ],
     }).compile()
 
     app = moduleRef.createNestApplication()
@@ -28,6 +38,8 @@ describe('Edit answer (E2E)', () => {
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
     answerFactory = moduleRef.get(AnswerFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
+    answerAttachmentFactory = moduleRef.get(AnswerAttachmentFactory)
     prisma = moduleRef.get(PrismaService)
     jwt = moduleRef.get(JwtService)
 
@@ -43,10 +55,25 @@ describe('Edit answer (E2E)', () => {
       authorId: user.id,
     })
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment()
+    const attachment2 = await attachmentFactory.makePrismaAttachment()
+
     const answer = await answerFactory.makePrismaAnswer({
       questionId: question.id,
       authorId: user.id,
     })
+
+    await answerAttachmentFactory.makePrismaAnswerAttachment({
+      answerId: answer.id,
+      attachmentId: attachment1.id,
+    })
+
+    await answerAttachmentFactory.makePrismaAnswerAttachment({
+      answerId: answer.id,
+      attachmentId: attachment2.id,
+    })
+
+    const attachment3 = await attachmentFactory.makePrismaAttachment()
 
     const answerId = answer.id.toString()
 
@@ -55,6 +82,7 @@ describe('Edit answer (E2E)', () => {
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
         content: 'New answer content',
+        attachments: [attachment1.id.toString(), attachment3.id.toString()],
       })
 
     expect(response.statusCode).toBe(204)
@@ -66,5 +94,19 @@ describe('Edit answer (E2E)', () => {
     })
 
     expect(answerOnDatabase).toBeTruthy()
+
+    const attachmentsOnDatabase = await prisma.attachment.findMany({
+      where: {
+        answerId: answerOnDatabase?.id,
+      },
+    })
+
+    expect(attachmentsOnDatabase).toHaveLength(2)
+    expect(attachmentsOnDatabase).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: attachment1.id.toString() }),
+        expect.objectContaining({ id: attachment3.id.toString() }),
+      ]),
+    )
   })
 })
