ee9d4ac2202646c6842b28612c7cb52bfca82143
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-12-12 10:53:47 -0300

    feat: redis cache implementation

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
-import { EnvService } from '../env/env.service'
 import { EnvModule } from '../env/env.module'
+import { CacheRepository } from './cache-repository'
+import { RedisCacheRepository } from './redis/redis-cache-repository'
+import { RedisService } from './redis/redis.service'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: RedisCacheRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
